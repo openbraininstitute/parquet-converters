82ae572b6a3e61b2177bca8ec25a7366562c3c1f
Fernando Pereira <fernando.pereira@epfl.ch>
2017-11-10 17:07:20 +0100

    Refcatored touch2parquet

--- touch2parquet.cpp
+++ touch2parquet.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <functional>
 #include <vector>
 #include <numeric>
@@ -6,20 +7,14 @@
 #include "parquetwriter.h"
 
 
+enum RunMode { QUIT_ERROR=-1, QUIT_OK, STANDARD, ENDIAN_SWAP };
+RunMode process_args(int argc, char* argv[]);
+
 
 int main( int argc, char* argv[] ) {
-
-    if( argc < 2) {
-        printf("usage: %s <touch_files ...>\n", argv[0]);
-        return 1;
-    }
-
-    //It will swap endians if we use the xxx_endian executable
-    int len = strlen(argv[0]);
-    bool swap_endians = false;
-    if( strcmp( &(argv[0][len-6]), "endian" ) == 0 ) {
-        swap_endians=true;
-        printf("[Info] Swapping endians\n");
+    RunMode mode = process_args(argc, argv);
+    if(mode < STANDARD) {
+        return mode;
     }
 
     ProgressHandler progress(argc-1);
@@ -27,7 +22,7 @@ int main( int argc, char* argv[] ) {
     #pragma omp parallel for
     for( int i=1; i<argc; i++) {
         printf("\n[Info] Loading %s\n", argv[i]);
-        Loader tl(argv[i], swap_endians);
+        Loader tl(argv[i], mode == ENDIAN_SWAP);
         string parquetFilename( argv[i] );
         std::size_t slashPos = parquetFilename.find_last_of("/\\");
         parquetFilename = parquetFilename.substr(slashPos+1);
@@ -46,4 +41,41 @@ int main( int argc, char* argv[] ) {
 
     printf("Done exporting\n");
     return 0;
+}
+
+
+RunMode process_args(int argc, char* argv[]) {
+
+    if( argc < 2) {
+        printf("usage: touch2parquet[_endian] <touch_file1 touch_file2 ...>\n");
+        printf("       touch2parquet [-h]\n");
+        return QUIT_ERROR;
+    }
+    if ( argv[1][0] == '-' ) {
+        //Handle options, especially -h, --help
+        // For the moment there are no options. We only show usage and quit.
+        printf("usage: touch2parquet[_endian] <touch_file1 touch_file2 ...>\n");
+        printf("       touch2parquet [-h]\n");
+        return QUIT_OK;
+    }
+
+    bool arg_error = false;
+    for( int i=1; i<argc; i++) {
+        if(access(argv[i] , F_OK) == -1) {
+            fprintf(stderr, "File '%s' doesn't exist, or no permission\n", argv[i]);
+            arg_error = true;
+        }
+    }
+    if(arg_error) {
+        return QUIT_ERROR;
+    }
+
+    //It will swap endians if we use the xxx_endian executable
+    int len = strlen(argv[0]);
+    if( len>6 && strcmp( &(argv[0][len-6]), "endian" ) == 0 ) {
+        printf("[Info] Swapping endians\n");
+        return ENDIAN_SWAP;
+    }
+
+    return STANDARD;
 }
