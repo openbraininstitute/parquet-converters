5acd6927a2061bc398b01b75d7910ef2b8e8d6f3
Fernando Pereira <fernando.pereira@epfl.ch>
2018-02-19 17:58:03 +0100

    Implementing h5 writing
    Change-Id: I8846d873dbc1b49fb51a72750ec4ab82cd3d541c

--- lib/circuit_writer_syn2.cpp
+++ lib/circuit_writer_syn2.cpp
@@ -1,16 +1,19 @@
 #include "circuit_writer_syn2.h"
 #include <functional>
 #include <thread>
+#include <hdf5.h>
+
 
 namespace neuron_parquet {
 namespace circuit {
 
 using namespace arrow;
+using namespace std;
 
 
-
-CircuitWriterSYN2::CircuitWriterSYN2(const std::string & destination_dir)
-    : destination_dir_(destination_dir)
+CircuitWriterSYN2::CircuitWriterSYN2(const string & destination_dir, uint64_t n_records)
+    : destination_dir_(destination_dir),
+      total_records_(n_records)
 { }
 
 
@@ -21,11 +24,19 @@ CircuitWriterSYN2::CircuitWriterSYN2(const std::string & destination_dir)
 /// \param length
 ///
 void CircuitWriterSYN2::write(const CircuitData * data, uint length) {
-    std::shared_ptr<Table> row_group(data->row_group);
+    if( data == nullptr || length == 0 ) {
+        shared_ptr<Column> _null;
+        for(auto & q : column_writer_queues_) {
+            q.blocking_push(_null);
+        }
+        return;
+    }
+
+    shared_ptr<Table> row_group(data->row_group);
     int n_cols = row_group->num_columns();
 
-    std::vector<int> cols_to_process(n_cols);
-    std::vector<int> remaining_cols;
+    vector<int> cols_to_process(n_cols);
+    vector<int> remaining_cols;
 
     for(int i=0; n_cols; i++) {
         cols_to_process.push_back(i);
@@ -54,13 +65,13 @@ void CircuitWriterSYN2::write(const CircuitData * data, uint length) {
 
                 column_writer_queues_.push_back(ZeroMemQ_Column());
 
-                h5 h5_file = init_h5file(std::string("syn2prop") + col_name, col);
+                h5_ids h5_file = init_h5file(string("syn2/") + col->name(), col);
                 create_thread_process_data(h5_file, column_writer_queues_.back());
             }
 
             ZeroMemQ_Column & q = column_writer_queues_[idx];
 
-            if(! q.try_push(col)) {
+            if( ! q.try_push(col)) {
                 //Thread not ready yet
                 remaining_cols.push_back(col_i);
             }
@@ -68,56 +79,125 @@ void CircuitWriterSYN2::write(const CircuitData * data, uint length) {
 
         if( remaining_cols.size()>0 ) {
             // Avoid sleep if no need
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            this_thread::sleep_for(chrono::milliseconds(1));
         }
 
         // Move also empties original
-        cols_to_process = std::move(remaining_cols);
+        cols_to_process = move(remaining_cols);
 
     }
 
 }
 
 
+h5_ids CircuitWriterSYN2::init_h5file(const string & path, shared_ptr<arrow::Column> column) {
+    hid_t file_id = H5Fcreate(path.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+    hsize_t dims[1] = {total_records_};
 
-template <typename T>
-inline void dump_data(h5 h5_file_handler, u_int8_t* buffer) {
+    hid_t dataspace_id = H5Screate_simple(1, dims, NULL);
+    Type::type t(column->type()->id());
 
+    hid_t ds_id = H5Dcreate2(file_id, "/dset", parquet_types_to_h5(t), dataspace_id,
+                      H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+    return {file_id, ds_id, dataspace_id};
 }
 
 
-inline void write_data(h5 h5_file,
-                       const std::shared_ptr<const Column>& col_data) {
-    // get data in buffer
-    for( const std::shared_ptr<Array> & chunk : col_data->data()->chunks() ) {
-        for( const std::shared_ptr<Buffer> & buf : chunk->data()->buffers ) {
-            (void) buf;
-        }
-    }
-}
 
-
-h5 init_h5file(const std::string & filename, std::shared_ptr<Column> column) {
-    return 0;
-}
-
-
-void CircuitWriterSYN2::create_thread_process_data(h5 h5_file,
+void CircuitWriterSYN2::create_thread_process_data(const h5_ids h5_output,
                                                    ZeroMemQ_Column & queue) {
-    auto f = [&h5_file, &queue]{
-        std::shared_ptr<Column> column_ptr = queue.blocking_pop();
+    auto f = [&h5_output, &queue]{
+        shared_ptr<Column> column_ptr;
+        uint64_t cur_offset = 0;
 
-        while(column_ptr != nullptr) {
-            write_data(h5_file, column_ptr);
-            column_ptr = queue.blocking_pop();
+        while((column_ptr = queue.blocking_pop()) != nullptr) {
+            write_data(h5_output, cur_offset, column_ptr);
         }
+        // close
+        H5Dclose(h5_output.ds);
+        H5Fclose(h5_output.file);
     };
 
-    std::thread t(f);
+    thread t(f);
     t.detach();
 }
 
 
+
+inline hid_t parquet_types_to_h5(Type::type t) {
+    switch( t ) {
+        case Type::INT8:
+            return H5T_STD_I8LE;
+        case Type::INT16:
+            return H5T_STD_I16LE;
+        case Type::INT32:
+            return H5T_STD_I32LE;
+        case Type::UINT8:
+            return H5T_STD_U8LE;
+        case Type::UINT16:
+            return H5T_STD_U16LE;
+        case Type::UINT32:
+            return H5T_STD_U32LE;
+        case Type::FLOAT:
+            return H5T_IEEE_F32LE;
+        case Type::DOUBLE:
+            return H5T_IEEE_F64LE;
+        case Type::STRING:
+            return H5T_C_S1;
+        default:
+            return -1;
+    }
+}
+
+inline uint parquet_types_size(Type::type t) {
+    switch( t ) {
+        case Type::INT8:
+        case Type::UINT8:
+            return 1;
+
+        case Type::INT16:
+        case Type::UINT16:
+            return 2;
+
+        case Type::INT32:
+        case Type::UINT32:
+        case Type::FLOAT:
+            return 4;
+
+        case Type::DOUBLE:
+            return 8;
+
+        default:
+            return -1;
+    }
+}
+
+
+inline void write_data(const h5_ids h5_ds, uint64_t& offset,
+                           const shared_ptr<const Column>& col_data) {
+
+    static thread_local Type::type t_id(col_data->type()->id());
+    static thread_local hid_t t = parquet_types_to_h5(t_id);
+    static thread_local uint elem_size = parquet_types_size(t_id);
+    static const hsize_t one = 1;
+    const hsize_t size = col_data->length();
+
+    // get data in buffers
+    for( const shared_ptr<Array> & chunk : col_data->data()->chunks() ) {
+
+        for( const shared_ptr<Buffer> & buf : chunk->data()->buffers ) {
+            const hsize_t offset_= offset;
+            uint64_t buf_len = chunk->length() / elem_size;
+            hid_t mem_space = H5Screate_simple(1, &size, nullptr);
+            hid_t file_space = H5Sselect_hyperslab(h5_ds.dspace, H5S_SELECT_SET, &offset_, &one, &size, NULL);
+            H5Dwrite(h5_ds.ds, t, mem_space, file_space, H5P_DEFAULT, buf->data());
+            offset += buf_len;
+            H5Sclose(file_space);
+            H5Sclose(mem_space);
+        }
+    }
+}
 
 
 
--- lib/circuit_writer_syn2.h
+++ lib/circuit_writer_syn2.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <unordered_map>
 #include <memory>
+#include <hdf5.h>
 
 namespace neuron_parquet {
 namespace circuit {
@@ -15,8 +16,10 @@ namespace circuit {
 
 typedef ZeroMemQ<arrow::Column> ZeroMemQ_Column;
 
-//debug
-typedef int h5;
+struct h5_ids {
+    hid_t file, ds, dspace;
+};
+
 
 ///
 /// \brief The CircuitWriterSYN2 which writes one hdf5 file per column.
@@ -26,7 +29,7 @@ typedef int h5;
 class CircuitWriterSYN2 : public Writer<CircuitData>
 {
 public:
-    CircuitWriterSYN2(const std::string & destination_dir);
+    CircuitWriterSYN2(const std::string & destination_dir, uint64_t n_records);
 
     virtual void write(const CircuitData* data, uint length) override;
 
@@ -34,21 +37,24 @@ private:
     // The writer can write to several files using different threads
     // and therefore release the main thread to go and fetch more data
 
-    void create_thread_process_data(h5 h5_file, ZeroMemQ_Column & queue);
+    h5_ids init_h5file(const std::string & filename, std::shared_ptr<arrow::Column> column);
+    void create_thread_process_data(h5_ids h5_output, ZeroMemQ_Column & queue);
 
     std::vector<ZeroMemQ_Column> column_writer_queues_;
     std::unordered_map<std::string, int> col_name_to_idx_;
 
     const std::string & destination_dir_;
+    const uint64_t total_records_;
+
 
 };
 
 
-// static
-h5 init_h5file(const std::string & filename, std::shared_ptr<arrow::Column> column);
+// Thread functions
+
+void write_data(h5_ids h5_ds, uint64_t offset, const std::shared_ptr<const arrow::Column>& col_data);
+inline hid_t parquet_types_to_h5(arrow::Type::type t);
 
 
-}
-}
-
+} }  // NS EOF
 #endif // CIRCUITWRITERSYN2_H
