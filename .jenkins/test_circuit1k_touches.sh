ddcc7a44e5124ad7ed5754f3413d7b87d65ccec5
Matthias Wolf <matthias.wolf@epfl.ch>
2018-09-04 13:40:29 +0200

    Spykfunc py3 module has been made the default.
    Change-Id: I6c2c29997db05414e64e5df63b622a84b1f18523

--- .jenkins/test_circuit1k_touches.sh
+++ .jenkins/test_circuit1k_touches.sh
@@ -3,7 +3,7 @@ srun -Aproj16 -pinteractive -n4 touch2parquetp \
 
 set +x
 hash module 2> /dev/null || . /etc/profile.d/modules.sh
-module load nix/hpc/spykfunc-py3
+module load nix/hpc/spykfunc
 set -x
 
 script=$(mktemp)
