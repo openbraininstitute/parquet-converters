e7e2b0380a76fca745457e62354949cf9f0adbd3
Matthias Wolf <matthias.wolf@epfl.ch>
2019-03-25 14:07:02 +0100

    change: fix CI scripts for easier target updates
    Change-Id: I2a84478c6f4e348a89d1e9973e6820c84a896efd

--- .jenkins/test_circuit1k_output.sh
+++ .jenkins/test_circuit1k_output.sh
@@ -1,6 +1,6 @@
 for mode in structural functional; do
-    srun -Aproj16 -pinteractive -n1 parquet2syn2p -o c.syn2 \
+    srun -Aproj16 -pinteractive -n1 parquet2syn2p -o ${mode}.syn2 \
         $DATADIR/cellular/circuit-1k/touches/$mode/circuit.parquet
-    h5diff c.syn2 \
+    h5diff ${mode}.syn2 \
         $DATADIR/cellular/circuit-1k/touches/$mode/circuit_from_parquet.syn2
 done
