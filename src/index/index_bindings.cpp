cc721482cd51720de4dd5c02331514b65f95fea4
Alexander Temerev <alexander.temerev@epfl.ch>
2024-09-10 13:32:13 +0200

    Add nanobind bindings for index writer C++ code and update CMake build

---
+++ src/index/index_bindings.cpp
@@ -0,0 +1,14 @@
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/string.h>
+#include <highfive/H5File.hpp>
+#include "index.h"
+
+namespace nb = nanobind;
+
+NB_MODULE(index_writer, m) {
+    m.def("write", [](const std::string& filename, uint64_t sourceNodeCount, uint64_t targetNodeCount) {
+        HighFive::File file(filename, HighFive::File::ReadWrite);
+        HighFive::Group root = file.getGroup("/");
+        indexing::write(root, sourceNodeCount, targetNodeCount);
+    }, "Write index to HDF5 file", nb::arg("filename"), nb::arg("sourceNodeCount"), nb::arg("targetNodeCount"));
+}
